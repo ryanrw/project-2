ce2b56a4c2c8ca5c7cc9630ab9812191a9f87a7d
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-22 20:32:10 +0700

    feat: add config

---
+++ monolith-backend/src/configs/index.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+import path from "path"
+
+const backendRootPath = path.join(__dirname, "../../")
+const projectPath = path.join(backendRootPath, "../")
+
+const EnvFile = dotenv.config()
+
+const noEnvFileFound = EnvFile.error
+
+if (noEnvFileFound) {
+  dotenv.config({
+    path: path.join(projectPath, ".env-sample"),
+  })
+}
+
+export default {
+  backendRootPath,
+  projectPath,
+  secret: process.env.SECRET,
+}
