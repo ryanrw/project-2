05f5ef96a96b263d66c38dfab776eb28a303a1cb
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-25 01:32:15 +0700

    feat: add login and jwt type

--- monolith-backend/src/graphql/typedefs/index.ts
+++ monolith-backend/src/graphql/typedefs/index.ts
@@ -1,8 +1,7 @@
 import root from "./root"
 import status from "./status"
-// @todo remove this if unuse
-import user from "./users"
 import createUser from "./users/create"
-import checkUser from "./users/check"
+import checkUser from "./users/login"
+import JWT from "./jwt"
 
-export const typeDefs = [root, status, user, createUser, checkUser]
+export const typeDefs = [root, status, createUser, checkUser, JWT]
--- monolith-backend/src/graphql/typedefs/users/login.ts
+++ monolith-backend/src/graphql/typedefs/users/login.ts
@@ -2,6 +2,6 @@ import { gql } from "apollo-server"
 
 export default gql`
   extend type Query {
-    checkUser(username: String!, password: String): Boolean!
+    login(username: String!, password: String): JWT!
   }
 `
