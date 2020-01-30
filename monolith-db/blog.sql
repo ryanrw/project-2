5369b7eec7b323a0dd7e0b592dd19c95c00d7041
Ryan-Willpower <surasak.c.work@gmail.com>
2020-01-30 21:17:26 +0700

    chore: update database structure

--- monolith-db/blog.sql
+++ monolith-db/blog.sql
@@ -4,7 +4,7 @@
 -- https://tableplus.com/
 --
 -- Database: blog
--- Generation Time: 2563-01-23 17:33:44.0360
+-- Generation Time: 2563-01-30 21:11:02.1830
 -- -------------------------------------------------------------
 
 
@@ -14,33 +14,6 @@ CREATE DATABASE blog;
 \c blog;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Table Definition
-CREATE TABLE "public"."contact" (
-    "email" varchar(64),
-    "tel" varchar(12)
-);
-
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Table Definition
-CREATE TABLE "public"."education" (
-    "name" varchar(64),
-    "description" text,
-    "year" varchar(32)
-);
-
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
--- Table Definition
-CREATE TABLE "public"."experience" (
-    "duration" varchar(32),
-    "place" varchar(128),
-    "job_title" varchar(64),
-    "description" text
-);
 
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 
@@ -54,7 +27,7 @@ CREATE TABLE "public"."posts" (
     "title" text,
     "excerpt" text,
     "content" text,
-    "postat" timestamp,
+    "postat" timestamp DEFAULT now(),
     PRIMARY KEY ("postid")
 );
 
@@ -62,24 +35,7 @@ CREATE TABLE "public"."posts" (
 
 -- Table Definition
 CREATE TABLE "public"."resume" (
-    "name" varchar(24),
-    "about_me" text,
-    "job_to_apply" text,
-    "github_link" varchar(64)
-);
-
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
-
-DROP TYPE IF EXISTS "public"."fieldlist";
-CREATE TYPE "public"."fieldlist" AS ENUM ('frontend', 'backend', 'other');
-DROP TYPE IF EXISTS "public"."skilllevel";
-CREATE TYPE "public"."skilllevel" AS ENUM ('expert', 'intermediate', 'beginner');
-
--- Table Definition
-CREATE TABLE "public"."tool_skills" (
-    "field" "public"."fieldlist",
-    "name" varchar(32),
-    "skill" "public"."skilllevel"
+    "metadata" jsonb
 );
 
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
@@ -94,4 +50,4 @@ CREATE TABLE "public"."users" (
     PRIMARY KEY ("userid")
 );
 
-ALTER TABLE "public"."posts" ADD FOREIGN KEY ("post_by") REFERENCES "public"."users"("userid");
+ALTER TABLE "public"."posts" ADD FOREIGN KEY ("postby") REFERENCES "public"."users"("userid");
